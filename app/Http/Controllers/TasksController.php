695be75cceb93f5db1d4f988079df1eec0c24ca5
delia <delia.gateje@webland.ro>
2021-05-28 22:33:45 +0300

    conflict tesr

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -29,7 +29,7 @@ class TasksController extends Controller
 
     public function store(Request $request)
     {
-        $title = $request->input('titleasdadasdas');
+        $title = $request->input('testest');
         $description = $request->input('description');
         $deadline = date('Y-m-d', strtotime($request->input('deadline')));
         $priority = $request->input('priority');
